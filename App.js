e0f3b900a2ea4e2866670f32f3d0ea846434ed12
Chandra.Shekar <chandra.shekar@wisetechglobal.com>
2023-11-21 09:41:15 +0530

    Added a bundler to the project

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
-/**
- * Nested Structure using React
- * 
- * <div id="parent">
- * 		<div id="children">
- * 			<h1>I'm h1 tag</h1>
- * 			<h2>I'm h2 tag</h2> // siblings
- * 		</div>
- * 		<div id="child2">
- * 			<h1>I'm h1 tag</h1>
- * 			<h2>I'm h2 tag</h2> // siblings
- * 		</div>
- * </div>
- * React Element(Object) => HTML(Browser Understands)   
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-// Nested Structures of React 
 const parent = React.createElement(
 	"div",
 	{ id: "parent" },
-	React.createElement("div", { id: "child" },
-		React.createElement("h1", {}, "I am an h1 tag"),
+	React.createElement("div", { id: "child" }, [
+		React.createElement("h1", {}, "This is Namaste React 🚀"),
 		React.createElement("h2", {}, "I am an h2 tag")
-	),
-	React.createElement("div", { id: "child2" },
-		React.createElement("h1", {}, "I am an h1 tag"),
+	]),
+	React.createElement("div", { id: "child2" }, [
+		React.createElement("h1", {}, "Hello"),
 		React.createElement("h2", {}, "I am an h2 tag")
-	)
+	])
 );
 
-//JSX
-
-// const heading = React.createElement(
-// 	"h1",
-// 	{ id: "heading" },
-// 	"Hello World from React!"
-// );
-
-console.log(parent); //object
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-#Namaste React - This is a mark up file.
+# Namaste React - This is a mark up file.
+# parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compress 
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - To Support older browsers.
+- Error Handling
+- HTTPs
+- Tree Shaking -- remove unused code 
+- Lazy Dev Builds  - Parcel can defer building files until they are requested in the browser.
+- Different dev and Prod Bundles
